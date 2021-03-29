820eafdb4677e694216f6547c209a675eafa938c
Aedan J. Andrews <andrewsa2@student.waihekehigh.school.nz>
2021-03-29 19:33:59 +1300

    Setup all files for MVP. Version 0.0.0

---
+++ tiled/loadingbay.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="loadingbay" tilewidth="1" tileheight="1" tilecount="0" columns="0">
+ <grid orientation="isometric" width="1" height="1"/>
+</tileset>
